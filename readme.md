5cb3e020871488dd3012dae9d67f2c1f010e6542
Jeff Seymour <seym1585@gmail.com>
2016-04-22 14:58:44 -0700

    update weather js file to prevet bug

--- readme.md
+++ readme.md
@@ -6,8 +6,7 @@
 ##### This is a simple ping-pong test built to experiment with node and other javascript tools.  It also includes a weather page to show current humidity in a given city.  
 ##### This is just an experimental repo to start learning and playing around with front-end tools such as Gulp, Bower, and APIs.
 
-###### [View on gh-pages](http://jeffsdev.github.io/nodejs-pingpong/)  
-Note: Weather may not work on gh-pages properly, as API key is stored in a local .env file. 
+Note: Weather may not work on gh-pages properly, as API key is stored in a local .env file.
 
 #### Technologies Used
 
@@ -20,6 +19,15 @@ Gulp
 Browserify
 Bower  
 
+### Installation
+Clone repository  
+```npm install```  
+```bower build```  
+```gulp build```  
+```gulp serve```  
+View in browser at localhost:3000  
+
 ##### License
+Copyright 2016 Jeff Seymour
 
 *This software is licensed under the MIT license.*
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -24,17 +24,23 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = require('./../.env').apiKey;
+// Note: this file has been commented out to prevent errors.
+// Requires API key for a weather service stored in a .env file
+// that won't be uploaded to github. Regarless, with a proper
+// API key, this weather functionality works.
 
 
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-      $('.showWeather').text(error.message);
-    });
-  });
-});
+// var apiKey = require('./../.env').apiKey;
+//
+//
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     var city = $('#location').val();
+//     $('#location').val("");
+//     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+//       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+//     }).fail(function(error) {
+//       $('.showWeather').text(error.message);
+//     });
+//   });
+// });
